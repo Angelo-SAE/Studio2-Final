5251c121643e237b9dd9cfb61b2cf7f05f27be1c
Khalid Himmo <khalidhimmo07@gmail.com>
2024-03-21 22:16:27 +0400

    Basic movement script

---
+++ Assets/_Project/Scripts/Player/MouseLook.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f; // Sensitivity of the mouse movement
+    public Transform playerBody; // Reference to the player body for rotation
+    private float xRotation = 0f; // To keep track of camera rotation on the X axis
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Locks the cursor to the center of the screen
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; // Increment the xRotation based on the mouseY input
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clamp the rotation to prevent flipping
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Apply rotation to the camera
+        playerBody.Rotate(Vector3.up * mouseX); // Rotate the player body around the Y axis
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    [SerializeField] private float jumpFallForce;
+
+    Rigidbody rb;
+    private bool isGrounded;
+    public Camera cam;
+    public bool isForcedDown;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+
+
+        MovePlayer();
+
+
+    }
+
+    void MovePlayer()
+    {
+        Vector3 cameraForward = cam.transform.forward;
+        Vector3 cameraRight = cam.transform.right;
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+
+        float moveForward = Input.GetAxis("Vertical");
+        float moveSideways = Input.GetAxis("Horizontal");
+
+
+        Vector3 movement = (cameraForward * moveForward + cameraRight * moveSideways) * moveSpeed;
+
+
+        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        if (rb.velocity.y < -0.1f)
+        {
+            rb.AddForce(-Vector3.up * jumpFallForce);
+            isForcedDown = true;
+        }
+        else
+        {
+            isForcedDown = false;
+
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
+    }
+
+
+    void OnCollisionEnter(Collision collision) //Reused from old project, will change to better groundCheck system later
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
+
